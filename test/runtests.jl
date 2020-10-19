75d337e101babe4f99eab791765f40ec5de5333f
Chua Lynn <chualynn@berkeley.edu>
2020-10-19 15:27:47 -0700

    Edit test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,11 @@ using LinearAlgebra
 @testset "Theta" begin
     include("theta_test.jl")
     include("lattice_test.jl")
+    include("ellipsoid_test.jl")
+    include("riemann_matrix_test.jl")
     include("characteristics_test.jl")
     include("accola_test.jl")
     include("fgsm_test.jl")
     include("schottky4_test.jl")
+    include("schottky_null_test.jl")
 end
